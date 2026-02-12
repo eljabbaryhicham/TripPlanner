0207a43c3e89bee51e8b25d2e0c6a4137267b4bb
DIGITAL ART <eljabaryhicham@gmail.com>
2026-02-12 11:34:38 +0000

    not updated !!! is there a way to upgrade it from terminal ?

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true
-    },
-    experimental: {
-        serverComponentsExternalPackages: ['@genkit-ai/google-genai', 'firebase-admin'],
-    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@genkit-ai/google-genai',
+      'firebase-admin',
+    ],
+  },
 };
 
 export default nextConfig;
