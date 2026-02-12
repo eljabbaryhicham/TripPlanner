590596def7ff097ccd6c0ab1082f28fb89673cee
DIGITAL ART <eljabaryhicham@gmail.com>
2026-02-12 11:45:42 +0000

    studio-5965912818:~/studio{main}$ npm install next@canary react@canary r

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // The `serverComponentsExternalPackages` option allows you to opt-out of bundling
-  // dependencies that are not compatible with the Edge Runtime.
-  // @see https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages
-  serverComponentsExternalPackages: [
-    '@genkit-ai/google-genai',
-    'firebase-admin',
-  ],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: [
+      'genkit',
+      '@genkit-ai/google-genai',
+      'firebase-admin',
+    ],
+  },
 };
 
 export default nextConfig;
