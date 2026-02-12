72fada69f3f4b76d5adb40bdbe580a8ca40571db
DIGITAL ART <eljabaryhicham@gmail.com>
2026-02-12 12:09:56 +0000

    is it impossible to upgrade next.js to 16.1.6 !!!???

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    // This is required for server-side libraries like Firebase Admin to work correctly.
     serverComponentsExternalPackages: ['firebase-admin'],
   },
 };
