d576e2503f5698299cedff59d38e9e611d4b0af5
DIGITAL ART <eljabaryhicham@gmail.com>
2026-02-12 11:37:56 +0000

    why you can't upgrade next.js to 16.1.6 !!

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: [
-      '@genkit-ai/google-genai',
-      'firebase-admin',
-    ],
-  },
+    experimental: {
+        serverComponentsExternalPackages: [
+            '@genkit-ai/google-genai',
+            'firebase-admin'
+        ],
+    },
 };
 
 export default nextConfig;
